87ad255825c788907d81d86b101133c6b3444e98
Anas Moin <anas.moin@contify.com>
2016-02-16 17:45:02 +0530

    initial commit of music

---
+++ music/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from gaana import views
+
+urlpatterns = [
+    url(r'^sign-up/$', views.sign_up, name='sign_up'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^login/$', views.logout_page, name='logout_page'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+]
