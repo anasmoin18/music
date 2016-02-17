61ee80fcfeee105565216475b79167d53ff3790c
Anas Moin <anas.moin@contify.com>
2016-02-17 16:44:29 +0530

    added urls for albums, artists & playlists

--- music/urls.py
+++ music/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from gaana import views
 
@@ -7,5 +7,10 @@ urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout_page'),
     url(r'^home/$', views.home, name='home'),
+    url(r'^albums/$', views.albums, name='albums'),
+    url(r'^artist/(?P<artist_id>\d+)/$', views.artist, name='artist'),
+    url(r'^album/(?P<album_id>\d+)/$', views.album, name='album'),
+    url(r'^playlists/$', views.playlists, name='playlists'),
+    url(r'^playlist/(?P<playlist_id>\d+)/$', views.playlist, name='playlist'),
     url(r'^admin/', include(admin.site.urls)),
 ]
