155bb446baeba8c70c5b0f95abdeb2850526e31e
Anas Moin <anas.moin@contify.com>
2016-02-17 16:01:30 +0530

    updated file

--- gaana/views.py
+++ gaana/views.py
@@ -1,11 +1,17 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.exceptions import *
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse, Http404
-from django.contrib.auth import logout
-from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import logout, login
+from gaana.models import *
 from forms import SignUpForm
 
-@csrf_protect
+
+def home(request):
+    return render_to_response('gaana/home.html',
+                              {'user': request.user})
+
+
 def sign_up(request):
     if request.method == 'POST':
         print request.POST
@@ -19,19 +25,51 @@ def sign_up(request):
     else:
         form = SignUpForm()
 
-    return render_to_response('gaana/sign_up.html', {'form': form},
+    return render_to_response('gaana/sign_up.html',
+                              {'form': form},
                               context_instance=RequestContext(request))
 
-@csrf_protect
-def login(request):
-    return render_to_response('registration/login.html', { 'user': request.user })
 
-@csrf_protect
+def login(request):
+    return render_to_response('registration/login.html',
+                              {'user': request.user})
+
+
+def albums(request):
+    albums = Album.objects.all().order_by('name')
+    return render_to_response('gaana/index.html',
+                              {'list': albums, 'view':'album'},
+                              context_instance=RequestContext(request))
+
+def artist(request,artist_id):
+    artist = Artist.objects.get(id=artist_id)
+    albums = Album.objects.filter(artist=artist)
+    return render_to_response('gaana/index.html',
+                              {'list': albums, 'view':'album', 'artist': artist},
+                              context_instance=RequestContext(request))
+
+def playlists(request):
+    playlists = Playlist.objects.all()
+    return render_to_response('gaana/index.html',
+                              {'list': playlists, 'view':'playlist'},
+                              context_instance=RequestContext(request))
+
+def playlist(request,playlist_id):
+    playlist = Playlist.objects.get(id=playlist_id)
+    tracks = [pc.track for pc in PlaylistConnection.objects.filter(playlist=playlist)]
+    return render_to_response('gaana/playlist.html',
+                              {'playlist': playlist, 'tracks': tracks, 'view': 'single_playlist'},
+                              context_instance=RequestContext(request))
+
+def album(request,album_id):
+    album = Album.objects.get(id=album_id)
+    tracks = Track.objects.filter(album=album).order_by('track_no')
+    return render_to_response('gaana/album.html',
+                              {'album': album, 'tracks': tracks, 'view': 'single_album'},
+                              context_instance=RequestContext(request))
+
 def logout_page(request):
     logout(request)
-    return HttpResponseRedirect('/home/')
+    return HttpResponseRedirect('/')
 
 
-@csrf_protect
-def home(request):
-    return render_to_response('gaana/home.html', { 'user': request.user })
